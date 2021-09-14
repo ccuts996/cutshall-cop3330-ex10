fd02d1fbc152e937676c20886e5792a4ff5b6af4
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 21:10:47 -0400

    Initial commit

---
+++ src/main/java/ex10.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class ex10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String item1;
+        System.out.print(" What is the quantity of item 1? ");
+
+        item1 = input.next();
+
+        Integer a = Integer.parseInt(item1);
+
+        String price1;
+        System.out.print(" What is the price of item 1? ");
+
+        price1 = input.next();
+
+        Integer b = Integer.parseInt(price1);
+
+        String item2;
+        System.out.print(" What is the quantity of item 2? ");
+
+        item2 = input.next();
+
+        Integer c = Integer.parseInt(item2);
+
+        String price2;
+        System.out.print(" What is the price of item 2? ");
+
+        price2 = input.next();
+
+        Integer d = Integer.parseInt(price2);
+
+        String item3;
+        System.out.print(" What is the quantity of item 3? ");
+
+        item3 = input.next();
+
+        Integer e = Integer.parseInt(item3);
+
+        String price3;
+        System.out.print(" What is the price of item 3? ");
+
+        price3 = input.next();
+
+        Integer f = Integer.parseInt(price3);
+
+        int subtotal = (a * b) + (c * d) + (e * f);
+        double salestax = subtotal * .055;
+        double total = subtotal + salestax;
+
+        System.out.print("Subtotal = " + subtotal + System.lineSeparator());
+        System.out.print("Sales Tax = " + salestax + System.lineSeparator());
+        System.out.print("Total = " + total + System.lineSeparator());
+
+    }
+}
